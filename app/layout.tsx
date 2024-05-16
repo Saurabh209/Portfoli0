007f24a26db8dfe22e2b689f238eba2ab1e7e3d1
Atom Code <112045222+AtomCode01@users.noreply.github.com>
2024-05-16 17:37:08 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "AtomCode | Personal Portfolio",
+  description: "Aman ( AtomCode ) expert in Data science, Machine leanring and MLops.",
 };
 
 export default function RootLayout({
