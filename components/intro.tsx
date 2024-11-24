cb7d653a259b46062b37fa0be1ab12f91da2036a
Atom Code <112045222+AtomCode01@users.noreply.github.com>
2024-11-24 19:09:15 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -90,7 +90,7 @@ export default function Intro() {
 
         <a
           id="download" className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/CV(3).pdf"
           download
         >
           Download CV{" "}
